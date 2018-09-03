593c6805fa4938275da6ec38cc3a5846b1831be6
emir_kartal <kartal.emirr@gmail.com>
2018-09-03 16:16:53 +0200

    Login Screen Add

---
+++ happyHour/View/LoginButton.swift
@@ -0,0 +1,21 @@
+//
+//  LoginButton.swift
+//  happyHour
+//
+//  Created by Emir Kartal on 2.09.2018.
+//  Copyright © 2018 Emir Kartal. All rights reserved.
+//
+
+import UIKit
+
+class LoginButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ happyHour/View/SignUpTextField.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpTextField.swift
+//  happyHour
+//
+//  Created by Emir Kartal on 2.09.2018.
+//  Copyright © 2018 Emir Kartal. All rights reserved.
+//
+
+import UIKit
+
+class SignUpTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
